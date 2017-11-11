8ea16aa9acbe760b3b76c67af5f5d3e9d21c51c2
Alexandre Resende <aar6947@gmail.com>
2017-11-11 19:00:09 -0200

    feature: Add cards routes nad controllers

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const expressSession = require('express-session');
 const helmet = require('helmet');
 
 const userRouter = require('./api/routes/user.routes');
+const cardRouter = require('./api/routes/card.routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,4 +15,5 @@ app
   .use(bodyParser.urlencoded({ extended: true}))
   .use(bodyParser.json())
   .use('/', userRouter)
+  .use('/:walletid/cards', cardRouter)
   .listen(port, () => console.log(`Server running on port ${port}`));
