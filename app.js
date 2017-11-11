03075ddcc4577e80019e14d505fa2fe1d45f6658
Alexandre Resende <aar6947@gmail.com>
2017-11-11 18:44:07 -0200

    feature: Add wallet routes and controllers
    - Add routes for getting wallets, add a wallet,
      editing a wallet and deleting a wallet and its
      respective controllers

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const expressSession = require('express-session');
 const helmet = require('helmet');
 
 const userRouter = require('./api/routes/user.routes');
+const walletRouter = require('./api/routes/wallet.routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,4 +15,5 @@ app
   .use(bodyParser.urlencoded({ extended: true}))
   .use(bodyParser.json())
   .use('/', userRouter)
+  .use('/wallet', walletRouter)
   .listen(port, () => console.log(`Server running on port ${port}`));
