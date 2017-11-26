a6842d1dabc3b0b3a1ccc74a766a42b7bc1e712f
Alexandre Resende <aar6947@gmail.com>
2017-11-25 23:27:49 -0200

    refactor: Add input validation and pre-conditions

--- api/controllers/wallet.controllers.js
+++ api/controllers/wallet.controllers.js
@@ -1,4 +1,6 @@
 
+const Joi = require('joi');
+
 const walletModel = require('../models-persistence/wallet.model');
 
 module.exports.getwallets = (req, res) => {
@@ -6,11 +8,37 @@ module.exports.getwallets = (req, res) => {
 };
 
 module.exports.addwallet = (req, res) => {
-  walletModel().addwallet(req, res, req.body);
+  const walletSchema = {
+    name: Joi.string().min(1).required()
+  }
+
+  const walletValidation = Joi.validate(req.body, walletSchema);
+
+  Promise
+    .all([walletValidation])
+    .then(() => {
+      walletModel().addwallet(req, res, req.body);
+    })
+    .catch((err) => {
+      res.status(500).send({ error: err.details[0].message });
+    });
 };
 
 module.exports.editwallet = (req, res) => {
-  walletModel().editwallet(req, res, req.body);
+  const walletSchema = {
+    name: Joi.string().min(1).required()
+  }
+
+  const walletValidation = Joi.validate(req.body, walletSchema);
+
+  Promise
+    .all([walletValidation])
+    .then(() => {
+      walletModel().editwallet(req, res, req.body);
+    })
+    .catch((err) => {
+      res.status(500).send({ error: err.details[0].message });
+    });
 };
 
 module.exports.deletewallet = (req, res) => {
@@ -22,5 +50,18 @@ module.exports.getlimit = (req, res) => {
 };
 
 module.exports.editlimit = (req, res) => {
-  walletModel().editlimit(req, res, req.body);
+  const limitSchema = {
+    limit: Joi.number().required()
+  }
+
+  const walletValidation = Joi.validate(req.body, limitSchema);
+
+  Promise
+    .all([walletValidation])
+    .then(() => {
+      walletModel().editlimit(req, res, req.body);
+    })
+    .catch((err) => {
+      res.status(500).send({ error: err.details[0].message });
+    });
 };
