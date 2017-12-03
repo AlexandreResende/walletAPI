4cd46343c320e501447ec0a08673be0d3a1f1ea6
Alexandre Resende <aar6947@gmail.com>
2017-12-03 08:38:41 -0200

    feature: Enable CORS in the project to external apps

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const port = process.env.PORT || 3000;
 
 app
   .use(helmet())
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
   .use(bodyParser.urlencoded({ extended: true}))
   .use(bodyParser.json())
   .use(userRouter)
