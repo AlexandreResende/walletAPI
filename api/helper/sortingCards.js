4fe2de196b0d493d327be8c324102313f32536f6
Alexandre Resende <aar6947@gmail.com>
2017-12-03 22:30:39 -0200

    feature: Add paymentRule file to project

--- api/helper/sortingCards.js
+++ api/helper/sortingCards.js
@@ -2,16 +2,16 @@ module.exports = (arrayOfCards) => {
   let orderedArray;
 
   orderedArray = arrayOfCards.sort( (firstValue, secondValue) => {
-    if (firstValue.dueDate > secondValue.dueDate){
-      return firstValue.dueDate - secondValue.dueDate;
-    } else if (firstValue.dueDate == secondValue.dueDate) {
+    if (firstValue.duedate > secondValue.duedate){
+      return firstValue.duedate - secondValue.duedate;
+    } else if (firstValue.duedate == secondValue.duedate) {
       if (firstValue.limit < secondValue.limit) {
         return firstValue.limit - secondValue.limit;
       } else {
         return secondValue.limit - firstValue.limit;
       }
     } else {
-      return secondValue.dueDate - firstValue.dueDate;
+      return secondValue.duedate - firstValue.duedate;
     }
   });
   return orderedArray;
