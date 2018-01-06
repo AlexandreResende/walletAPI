f4deead1a259532d446d1fde160e50387d29217a
Alexandre Resende <aar6947@gmail.com>
2018-01-06 14:47:55 -0200

    refactor: Update wallet files and verification

--- api/routes/wallet.routes.js
+++ api/routes/wallet.routes.js
@@ -3,18 +3,25 @@ const express = require('express');
 
 const router = express.Router();
 
-const walletController = require('../controllers/wallet.controllers');
+const {
+  getwallets,
+  addwallet,
+  editwallet,
+  deletewallet,
+  getlimit,
+  editlimit,
+} = require('../controllers/wallet.controllers');
 
-router.get('/:userid/wallet', walletController.getwallets);
+router.get('/:userid/wallet', getwallets);
 
-router.post('/:userid/wallet', walletController.addwallet);
+router.post('/:userid/wallet', addwallet);
 
-router.put('/:userid/:walletid', walletController.editwallet);
+router.put('/:userid/:walletid', editwallet);
 
-router.delete('/:userid/:walletid', walletController.deletewallet);
+router.delete('/:userid/:walletid', deletewallet);
 
-router.get('/:userid/:walletid/limit', walletController.getlimit);
+router.get('/:userid/:walletid/limit', getlimit);
 
-router.put('/:userid/:walletid/limit', walletController.editlimit);
+router.put('/:userid/:walletid/limit', editlimit);
 
 module.exports = router;
