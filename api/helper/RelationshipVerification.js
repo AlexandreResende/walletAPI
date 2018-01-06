24c195aa9749f19e8861ba825f685ea005e90d91
Alexandre Resende <aar6947@gmail.com>
2018-01-06 14:46:14 -0200

    refactor: Change RelationshipVerification

--- api/helper/RelationshipVerification.js
+++ api/helper/RelationshipVerification.js
@@ -5,64 +5,60 @@ const UserVerification = require('./UserVerification');
 const CardVerification = require('./CardVerification');
 const WalletVerification = require('./WalletVerification');
 
-class Verification {
-  static isUserWalletRelationshipValid(userId, walletId) {
-    return new Promise((resolve, reject) => {
-      const isUserRegistered = UserVerification.isUserValid(userId);
-      const isWalletRegistered = WalletVerification.isWalletValid(walletId);
+module.exports.isUserWalletRelationshipValid = (userId, walletId) => {
+  return new Promise((resolve, reject) => {
+    const isUserRegistered = UserVerification.isUserValid(userId);
+    const isWalletRegistered = WalletVerification.isWalletValid(walletId);
 
-      Promise
-        .all([isUserRegistered, isWalletRegistered])
-        .then((result) => {
-          models.wallet.findOne({
-            where: {
-              id: walletId,
-            },
-          })
-            .then((result) => {
-              if (result.userid !== userId) {
-                reject('User wallet relationship does not exist');
-              }
-              resolve();
-            })
-            .catch((err) => {
-              reject(err);
-            });
+    Promise
+      .all([isUserRegistered, isWalletRegistered])
+      .then((result) => {
+        models.wallet.findOne({
+          where: {
+            id: walletId,
+          },
         })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  }
-
-  isWalletCardRelationshipValid(userId, walletId, cardId) {
-    return new Promise((resolve, reject) => {
-      const isCardRegistered = CardVerification.isCardValid(cardId);
-      const isisUserWalletRelationshipValid = this.isUserWalletRelationshipValid(userId, walletId);
-
-      Promise
-        .all([isCardRegistered, isisUserWalletRelationshipValid])
-        .then((result) => {
-          models.cards.findOne({
-            where: {
-              id: cardId,
-            },
+          .then((result) => {
+            if (result.userid.toString() !== userId) {
+              reject('User wallet relationship does not exist');
+            }
+            resolve();
           })
-            .then((result) => {
-              if (result.walletid !== walletId) {
-                reject('Wallet card relationship does not exist');
-              }
-              resolve();
-            })
-            .catch((err) => {
-              reject(err);
-            });
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  }
+          .catch((err) => {
+            reject(err);
+          });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
-module.exports = () => new Verification();
+module.exports.isWalletCardRelationshipValid = (userId, walletId, cardId) => {
+  return new Promise((resolve, reject) => {
+    const isCardRegistered = CardVerification.isCardValid(cardId);
+    const isisUserWalletRelationshipValid = this.isUserWalletRelationshipValid(userId, walletId);
+
+    Promise
+      .all([isCardRegistered, isisUserWalletRelationshipValid])
+      .then((result) => {
+        models.cards.findOne({
+          where: {
+            id: cardId,
+          },
+        })
+          .then((result) => {
+            if (result.walletid !== walletId) {
+              reject('Wallet card relationship does not exist');
+            }
+            resolve();
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
