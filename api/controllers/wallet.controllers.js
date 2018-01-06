2d0b55f81ed91c5e2d2796a9ea204c5e72c74a78
Alexandre Resende <aar6947@gmail.com>
2018-01-05 22:35:35 -0200

    refactor: Adjust controllers based on models changes

--- api/controllers/wallet.controllers.js
+++ api/controllers/wallet.controllers.js
@@ -4,12 +4,12 @@ const Joi = require('joi');
 const walletModel = require('../models-persistence/wallet.model');
 
 module.exports.getwallets = (req, res) => {
-  walletModel().getwallets(req, res);
+  walletModel.getwallets(req, res);
 };
 
 module.exports.addwallet = (req, res) => {
   const walletSchema = {
-    name: Joi.string().min(1).required()
+    name: Joi.string().min(1).required(),
   }
 
   const walletValidation = Joi.validate(req.body, walletSchema);
@@ -17,7 +17,7 @@ module.exports.addwallet = (req, res) => {
   Promise
     .all([walletValidation])
     .then(() => {
-      walletModel().addwallet(req, res, req.body);
+      walletModel.addwallet(req, res, req.body);
     })
     .catch((err) => {
       res.status(500).send({ error: err.details[0].message });
@@ -26,7 +26,7 @@ module.exports.addwallet = (req, res) => {
 
 module.exports.editwallet = (req, res) => {
   const walletSchema = {
-    name: Joi.string().min(1).required()
+    name: Joi.string().min(1).required(),
   }
 
   const walletValidation = Joi.validate(req.body, walletSchema);
@@ -34,7 +34,7 @@ module.exports.editwallet = (req, res) => {
   Promise
     .all([walletValidation])
     .then(() => {
-      walletModel().editwallet(req, res, req.body);
+      walletModel.editwallet(req, res, req.body);
     })
     .catch((err) => {
       res.status(500).send({ error: err.details[0].message });
@@ -42,11 +42,11 @@ module.exports.editwallet = (req, res) => {
 };
 
 module.exports.deletewallet = (req, res) => {
-  walletModel().deletewallet(req, res);
+  walletModel.deletewallet(req, res);
 };
 
 module.exports.getlimit = (req, res) => {
-  walletModel().getlimit(req, res);
+  walletModel.getlimit(req, res);
 };
 
 module.exports.editlimit = (req, res) => {
@@ -59,7 +59,7 @@ module.exports.editlimit = (req, res) => {
   Promise
     .all([walletValidation])
     .then(() => {
-      walletModel().editlimit(req, res, req.body);
+      walletModel.editlimit(req, res, req.body);
     })
     .catch((err) => {
       res.status(500).send({ error: err.details[0].message });
