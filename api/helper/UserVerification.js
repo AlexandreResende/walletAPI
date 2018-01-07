23bc3fc29ec0dda951ccb4663c9182eab8554add
Alexandre Resende <aar6947@gmail.com>
2018-01-07 15:21:42 -0200

    refactor: Change verification modules to static classes

--- api/helper/UserVerification.js
+++ api/helper/UserVerification.js
@@ -1,66 +1,70 @@
 
 const models = require('../models');
 
-module.exports.isUserValid = (userId) => {
-  return new Promise((resolve, reject) => {
-    models.user.findOne({
-      where: {
-        id: userId,
-      },
-    })
-      .then((result) => {
-        if (result === null) {
-          reject('User does not exist');
-        }
-        resolve();
-      });
-  });
-}
-
-module.exports.isEmailUsed = (email) => {
-  return new Promise((resolve, reject) => {
-    models.user.findOne({
-      where: {
-        email,
-      },
-    })
-      .then((result) => {
-        if (result === null) {
+class UserVerification {
+  static isUserValid(userId) {
+    return new Promise((resolve, reject) => {
+      models.user.findOne({
+        where: {
+          id: userId,
+        },
+      })
+        .then((result) => {
+          if (result === null) {
+            reject('User does not exist');
+          }
           resolve();
-        }
-        reject('Email already in use');
+        });
+    });
+  }
+
+  static isEmailUsed(email) {
+    return new Promise((resolve, reject) => {
+      models.user.findOne({
+        where: {
+          email,
+        },
       })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-}
-
-module.exports.hasUserWallets = (userId) => {
-  return new Promise((resolve, reject) => {
-    const isUserRegistered = this.isUserValid(userId);
-
-    Promise
-      .all([isUserRegistered])
-      .then((result) => {
-        models.wallet.find({
-          where: {
-            userid: userId,
-          },
-        })
-          .then((result) => {
-            console.log(result);
-            if (result !== null) {
-              reject('You need to delete your wallets before');
-            }
+        .then((result) => {
+          if (result === null) {
             resolve();
+          }
+          reject('Email already in use');
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  static hasUserWallets(userId) {
+    return new Promise((resolve, reject) => {
+      const isUserRegistered = this.isUserValid(userId);
+
+      Promise
+        .all([isUserRegistered])
+        .then((result) => {
+          models.wallet.find({
+            where: {
+              userid: userId,
+            },
           })
-          .catch((err) => {
-            reject(err);
-          });
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+            .then((result) => {
+              console.log(result);
+              if (result !== null) {
+                reject('You need to delete your wallets before');
+              }
+              resolve();
+            })
+            .catch((err) => {
+              reject(err);
+            });
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
 }
+
+module.exports = UserVerification;
